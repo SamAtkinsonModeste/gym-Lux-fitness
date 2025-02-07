4ed398fbae277add707fa52c4e7697d3882de2b6
Samantha Atkinson <sammodeste@yahoo.com>
2025-02-07 16:42:06 +0000

    security of database  added

--- luxfitness/settings.py
+++ luxfitness/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zmmdhj@l&(b5n1+u+7s$sz@-=+7)c^7ni)!^*-b*s9&gk=uwdm'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 

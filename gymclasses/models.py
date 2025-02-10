4592ca28d78c7a9b97d9f6175b3afe9ec4b73140
Samantha Atkinson <sammodeste@yahoo.com>
2025-02-10 17:26:24 +0000

    model: changed variable field name for gymclases model

--- gymclasses/models.py
+++ gymclasses/models.py
@@ -11,7 +11,7 @@ class GymClasses(models.Model):
     slug = models.SlugField(max_length=200)
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name="gymclasses")
-    description = models.TextField()
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=STATUS, default="0")
     excert = models.TextField(blank=True)

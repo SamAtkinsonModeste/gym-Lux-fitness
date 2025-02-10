77a7189862b58ceec91a2d020dde9188353f6c33
Samantha Atkinson <sammodeste@yahoo.com>
2025-02-10 03:24:13 +0000

    created 1st custom model for list of gym classes

--- gymclasses/models.py
+++ gymclasses/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = (("0", "Draft"), ("1", "Published"))
+
+
+class GymClasses(models.Model):
+    name = models.CharField(max_length=200, unique=True),
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="gymclasses")
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS, default="0")
+    excert = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Class: {self.name} created on {self.created_on} last updated on {self.updated_on}"

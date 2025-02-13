a55399ac3ba8f68579a1552e3b77f9ad6118ca57
Samantha Atkinson <sammodeste@yahoo.com>
2025-02-13 23:03:25 +0000

    model: deleted and re-did my gymclasses model

--- gymclasses/models.py
+++ gymclasses/models.py
@@ -3,14 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-STATUS = (("0", "Draft"), ("1", "Published"))
+STATUS = (("0", "Draft"), ("1", "Publish"))
 
 
 class GymClasses(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, related_name="gymclasses")
+        User, on_delete=models.SET_NULL, null=True, related_name='gymclasses')
+    name = models.CharField(max_length=200, blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=STATUS, default="0")
@@ -18,7 +19,7 @@ class GymClasses(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ['-created_on']
 
     def __str__(self):
-        return f"Class: {self.title} created on {self.created_on}"
+        return f" {self.title} - {self.created_on}"
